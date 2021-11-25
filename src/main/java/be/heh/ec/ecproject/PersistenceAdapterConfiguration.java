be06c2430a08f47ebfd03f4f7c7a4ec1706dff9a
DemoNiiaKz <47399755+DemoNiiaKz@users.noreply.github.com>
2021-11-25 10:00:27 +0100

    EG1-4 Afficher la liste des produits

--- src/main/java/be/heh/ec/ecproject/PersistenceAdapterConfiguration.java
+++ src/main/java/be/heh/ec/ecproject/PersistenceAdapterConfiguration.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-
+@Profile("dev")
 @Configuration
 @EnableJpaRepositories
 public class PersistenceAdapterConfiguration {
--- src/main/resources/db/migration/V1__createTable.sql
+++ src/main/resources/db/migration/V1__createTable.sql
@@ -1,7 +1,7 @@
-CREATE TABLE IF NOT EXISTS cars(
-    id   SERIAL,
-    surname varchar(15),
-    mark varchar(15),
-    price int,
-    description varchar(15)
-    );
+create table IF NOT EXISTS cars (
+                      id INT,
+                      surname VARCHAR(50),
+                      mark VARCHAR(50),
+                      price INT,
+                      description VARCHAR(100)
+);
