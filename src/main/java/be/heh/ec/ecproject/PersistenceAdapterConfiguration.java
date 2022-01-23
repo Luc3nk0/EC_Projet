26310ce6097dda39177d142da5bb7d43b65991af
Benjistd <benjamin.cacciato@std.heh.be>
2022-01-23 14:23:46 +0100

    Sprint 3 clean

--- src/main/java/be/heh/ec/ecproject/PersistenceAdapterConfiguration.java
+++ src/main/java/be/heh/ec/ecproject/PersistenceAdapterConfiguration.java
@@ -16,7 +16,7 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@Profile("dev")
+@Profile("prod")
 @Configuration
 @EnableJpaRepositories
 public class PersistenceAdapterConfiguration {
